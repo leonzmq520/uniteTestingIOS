7cd1fe909078e6875ce895ddc509a1667982f49e
maoqiao <leon_zmq520@hotmail.com>
2014-10-21 12:50:40 +0200

    Initial Commit

---
+++ uniteTestingIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  uniteTestingIOS
+//
+//  Created by leon on 14/10/21.
+//  Copyright (c) 2014年 leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ uniteTestingIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  uniteTestingIOS
+//
+//  Created by leon on 14/10/21.
+//  Copyright (c) 2014年 leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
